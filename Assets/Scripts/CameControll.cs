66b490bc12924f3777f8a77c38d96ded6abf096f
Thmaster7 <theorocha72@gmail.com>
2025-03-10 17:49:39 -0300

    CarMoving

--- Assets/Scripts/CameControll.cs
+++ Assets/Scripts/CameControll.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class Wheel : MonoBehaviour
+public class CameControll : MonoBehaviour
 {
-    
+    public CarBody car;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +12,6 @@ public class Wheel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(car.transform.position.x, car.transform.position.y + 0.5f, car.transform.position.z - 5);
     }
 }
--- Assets/Scripts/CarBody.cs
+++ Assets/Scripts/CarBody.cs
@@ -14,6 +14,61 @@ public class CarBody : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float move = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+            transform.position += transform.forward * velocity;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+        }
+        if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y - rotation * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+            transform.Rotate(Vector3.down * rotation);
+            transform.position = new Vector3(transform.position.x - velocity * Time.deltaTime, transform.position.y, transform.position.z + velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        {
+            //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + rotation * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+            transform.Rotate(Vector3.up * rotation);
+            transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z + velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+            
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x - velocity * Time.deltaTime, transform.position.y, transform.position.z - velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z - velocity * Time.deltaTime);
+            wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
+            wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
+            wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
+            wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+        }
         
     }
 }

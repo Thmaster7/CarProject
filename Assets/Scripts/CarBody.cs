7748360ad596a289dc4cb939eb4bc32858f361b9
Thmaster7 <theorocha72@gmail.com>
2025-03-11 19:24:54 -0300

    Somechanges

--- Assets/Scripts/CarBody.cs
+++ Assets/Scripts/CarBody.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CarBody : MonoBehaviour
 {
-    public Wheel wheel1, wheel2, wheel3, wheel4;
+    [SerializeField] List<Wheel> wheelsColliders;
     public Wheel wheelMesh1, wheelMesh2, wheelMesh3, wheelMesh4;
     private float velocity = 20f;
     private float rotation = 50f;
@@ -35,68 +36,65 @@ public class CarBody : MonoBehaviour
 
             if (Input.GetKey(KeyCode.W))
             {
-                //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + velocity * Time.deltaTime);
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
-                transform.position += transform.forward * velocity * Time.deltaTime;
+            
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
+            }
+            transform.position += transform.forward * velocity * Time.deltaTime;
             
             }
             if (Input.GetKey(KeyCode.S))
             {
-                //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - velocity * Time.deltaTime);
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
-                transform.position -= transform.forward * velocity * Time.deltaTime;
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
+            }
+            transform.position -= transform.forward * velocity * Time.deltaTime;
             }
             if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
             {
-            //transform.position = new Vector3(transform.position.x - velocity * Time.deltaTime, transform.position.y, transform.position.z + velocity * Time.deltaTime);
-            //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y - rotation * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+            
             transform.Rotate(Vector3.down * rotation * Time.deltaTime);
-                
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
-                wheelMesh1.transform.Rotate(Vector3.down * rotation * Time.deltaTime);
+
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
+            }
+            wheelMesh1.transform.Rotate(Vector3.down * rotation * Time.deltaTime);
                 wheelMesh2.transform.Rotate(Vector3.down * rotation * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
             {
-            //transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z + velocity * Time.deltaTime);
-            //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + rotation * Time.deltaTime, transform.rotation.z, transform.rotation.w);
+            
             transform.Rotate(Vector3.up * rotation * Time.deltaTime);
-                
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
-                wheelMesh1.transform.Rotate(Vector3.up * rotation * Time.deltaTime);
+
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
+            }
+            wheelMesh1.transform.Rotate(Vector3.up * rotation * Time.deltaTime);
                 wheelMesh2.transform.Rotate(Vector3.up * rotation * Time.deltaTime);
 
             }
             if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
             {
-                //transform.position = new Vector3(transform.position.x - velocity * Time.deltaTime, transform.position.y, transform.position.z - velocity * Time.deltaTime);
+                
                 transform.Rotate(Vector3.up * rotation * Time.deltaTime);
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
             }
+        }
             if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
             {
-                //transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z - velocity * Time.deltaTime);
+                
                 transform.Rotate(Vector3.down * rotation * Time.deltaTime);
-                wheel1.transform.position = new Vector3(wheel1.transform.position.x, wheel1.transform.position.y, wheel1.transform.position.z);
-                wheel2.transform.position = new Vector3(wheel2.transform.position.x, wheel2.transform.position.y, wheel2.transform.position.z);
-                wheel3.transform.position = new Vector3(wheel3.transform.position.x, wheel3.transform.position.y, wheel3.transform.position.z);
-                wheel4.transform.position = new Vector3(wheel4.transform.position.x, wheel4.transform.position.y, wheel4.transform.position.z);
+            foreach (var wheel in wheelsColliders)
+            {
+                wheel.transform.position = wheel.transform.position;
             }
+        }
             if (Input.GetKey(KeyCode.F))
             {
                 transform.position = initialPos;
@@ -128,7 +126,7 @@ public class CarBody : MonoBehaviour
     }
     void CheckIfCanMove()
     {
-        if(wheel1.IsOnGround()|| wheel2.IsOnGround()|| wheel3.IsOnGround()|| wheel4.IsOnGround())
+        /*if(wheel1.GetComponent<WheelCollider>(isGrounded)|| wheel2.IsOnGround()|| wheel3.IsOnGround()|| wheel4.IsOnGround())
         {
             canMove = true;
         }
@@ -136,7 +134,7 @@ public class CarBody : MonoBehaviour
         {
             canMove = false;
             
-        }
+        }*/
     }
 
     
